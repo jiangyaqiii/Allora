52e39e3d1063d5114739c20a1a114242da508f38
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-14 01:47:26 +0800

    Update add_wallet.sh

--- add_wallet.sh
+++ add_wallet.sh
@@ -6,6 +6,7 @@ rm -rf /usr/local/go
 curl -OL https://go.dev/dl/go1.22.4.linux-amd64.tar.gz 
 sudo tar -C /usr/local -xvf go1.22.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
+source ~/.bashrc
 go version
 
 #安装allora钱包
